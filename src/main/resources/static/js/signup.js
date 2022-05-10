e0b08cd7c3e6f07f4b7a1b7e4b3bcd316e47fcf2
jang6129 <jang6129@naver.com>
2022-05-10 20:02:23 +0900

    update

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('#btn').click(function() {
+    $('#btnSignup').click(function() {
         var inputId = $('#id').val();
         var inputPwd = $('#pwd').val();
         var inputName = $('#name').val();
@@ -14,31 +14,11 @@ $(document).ready(function(){
             data: query,
             contentType: "application/x-www-form-urlencoded; charset=UTF-8",
 
-            success: function (res) {
-                alert(res);
+            success: function (json) {
+                $('#tbody').append(query);
             }
         })
     })
 
-    function singUp() {
-        $.ajax( {
-            type: "POST",
-            url: "http://192.168.0.39:8080/v1/user/signup",
-            contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-
-            success: function (json) {
-                var list = json;
-                var listLen =json.length;
-                for (var i = 0; i < listLen; i++) {
-                    var str = "<tr>" +
-                        "<td>" + list[i].id + "</td>" +
-                        "<td>" + list[i].pwd + "</td>" +
-                        "<td>" + list[i].name + "</td>" +
-                        "</tr>";
-                    $('#tbody').append(str);
-                }
-            }
-        })
-    }
 
 })
